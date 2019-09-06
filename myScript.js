fab73371da2073d1eb836053dad22ba0f1284d93
Kylan O'Connor <kylan02@gmail.com>
2019-09-06 10:01:41 -0700

    added to thanos buttons

--- myScript.js
+++ myScript.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	var x = 0;
 	$("#hide").click(function(){
 		$("p").hide();
 	});
@@ -6,6 +7,9 @@ $(document).ready(function(){
 		$("p").show();
 	});
 	$("#thanos").click(function(){
+		x=x+1
 		$(".first").toggle();
+		$(".stl").css({"color":"red", "background-color":"white"})
+		$("body").css({"background-color":"black"})
 	});
 });
